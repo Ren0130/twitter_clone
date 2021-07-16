7c9ea44d5d0fd50251a25e722cbd8c761756ea7c
ren <blueren27@gmail.com>
2021-07-16 12:39:18 +0000

    user登録ができるまで

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protect_from_forgery with: :exception
   add_flash_types :success, :info, :warning, :danger
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
