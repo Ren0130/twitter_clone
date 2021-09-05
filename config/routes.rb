403ed92af620d4b30cbedc5b873de80c90db18ca
ren <blueren27@gmail.com>
2021-09-05 13:02:40 +0000

    ナビゲーション実装によるCSS修正・その他VViewの見た目変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   resources :topics, only: %i[index new create]
   devise_for :users, module: :users
   root "roots#index"
+
+  get 'favorites/index'
+  post '/favorites', to: 'favorites#create'
 end
