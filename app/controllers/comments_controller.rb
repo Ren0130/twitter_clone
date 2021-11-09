e6a03ec6694ef0bcccbed5bf907b9975abdc6568
ren <blueren27@gmail.com>
2021-11-09 05:07:19 +0000

    アクション時の遷移画面を変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,17 +2,15 @@ class CommentsController < ApplicationController
 
   def create
     if Comment.new(comment_params).save
-      redirect_to topics_path, success: 'コメントを投稿しました'
+      redirect_to detail_topics_path(topic_id: comment_params[:topic_id]), success: 'コメントを投稿しました'
     else
-      redirect_to topics_path, danger: 'コメントの投稿に失敗しました'
+      redirect_to detail_topics_path(topic_id: comment_params[:topic_id]), danger: 'コメントの投稿に失敗しました'
     end
   end
 
   private
 
   def comment_params
-    {
-      user_id: current_user.id,
-    }.merge(params.require(:comment).permit(:topic_id, :description))
+    params.require(:comment).permit(:topic_id, :description).merge(user_id: current_user.id)
   end
 end
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -9,9 +9,20 @@ class FavoritesController < ApplicationController
     favorite.topic_id = params[:topic_id]
 
     if favorite.save
-      redirect_to topics_path, success: 'お気に入りに登録しました'
+      redirect_to detail_topics_path(topic_id: params[:topic_id]), success: 'お気に入りに登録しました'
     else
-      redirect_to topics_path, danger: 'お気に入りに登録に失敗しました'
+      redirect_to detail_topics_path(topic_id: params[:topic_id]), danger: 'お気に入りに登録に失敗しました'
     end
   end
+
+  def destroy
+    favorite = Favorite.find_by(user_id: current_user.id, topic_id: params[:topic_id])
+
+    if favorite.present? && favorite.destroy
+      redirect_to detail_topics_path(topic_id: params[:topic_id]), success: 'お気に入りに取り消しました'
+    else
+      redirect_to detail_topics_path(topic_id: params[:topic_id]), danger: 'お気に入りの取り消しに失敗しました'
+    end
+  end
+
 end
